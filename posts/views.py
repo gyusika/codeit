1ce44d9ec40a0a2e316dfab11059c50c1f39a8b2
정규식 <jung-gyusik@jeong-gyusig-ui-MacBookPro-2.local>
2022-03-08 18:19:27 +0900

    first commit

---
+++ posts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+# def index(requests):
+#     return render(requests, )
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/post_list.html', {"posts" : posts})
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'posts/post_detail.html', {"post" : post})
+
+def post_create(request):
+    if request.method == "POST":
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            new_post = post_form.save()
+            return redirect('post-detail', post_id=new_post.id)
+    else:
+        post_form = PostForm()
+    return render(request, 'posts/post_form.html', {"form" : post_form})
